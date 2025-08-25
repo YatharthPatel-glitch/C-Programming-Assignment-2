4c41151350f9eefff6ed94202ffe527094556a9a
YatharthPatel-glitch <yatharth2070@gmail.com>
2025-08-25 15:30:54 +0530

    Add files via upload

---
+++ net salary.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() {
+float gs;          //gs = gross salary
+float ns;          //ns = net salary
+float allowance;
+float deduction;
+
+printf("Enter the gross salary : ");
+scanf("%f", &gs);
+if(gs > 10000){
+allowance = 0.10 * gs;
+deduction = 0.03 * gs;
+}
+else if(gs > 5000){
+allowance = 0.07 * gs;
+deduction = 0.02 * gs;
+}
+else{
+allowance = 0.05* gs;
+deduction = 0.02* gs;
+}
+
+ns = gs + allowance - deduction;
+
+printf("Net salary = %f\n",ns);
+
+return 0;
+}
