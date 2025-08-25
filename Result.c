3458e984ac399d176f370542aadc8002fb5337b2
YatharthPatel-glitch <yatharth2070@gmail.com>
2025-08-25 15:23:01 +0530

    Update Result.c

--- Result.c
+++ Result.c
@@ -1,31 +1,43 @@
 #include<stdio.h>
+
 int main() {
+int maths;
+int chemistry;
+int physics;
 
-float a, b, c, d;
+printf("Enter the marks of maths : ");
+scanf("%d" , &maths);
+printf("Enter the marks of chemistry : ");
+scanf("%d" , &chemistry);
+printf("Enter the marks of physics : ");
+scanf("%d" , &physics);
+  
+int total  = maths + chemistry + physics;
+float avg = (maths + chemistry + physics)/3;
+  
+printf("Total of three subject = %d\n",total);
+printf("Average of three subject = %f \n",avg);
 
-printf("Enter marks: ");
-scanf("%f", &a);
+if(maths < 35 || chemistry < 35 || physics < 35){
+printf("Better luck next time ! You are fail.\n");
+}
+else{
+if(avg >= 70){
+printf("Congratulation , You got distinction\n");
+}
+else if(avg >= 60){
+printf("Congratulations , You got first class.\n");
+}
+else if(avg >= 50){
+printf("Congratulations , You got second class.\n");
+}
+else if(avg >= 35){
+printf("Congratulations,You got third class.\n");
+}
+else{
+printf("Better luck next time ! You are fail.\n");
+}
+}
 
-if(a > 70)
-{
-    printf("Distinction");
-}
-else if(a > 60 && a < 70)
-{
-    printf("First");
-}
-else if(a > 50 && a < 60)
-{
-    printf("Second");
-}
-else if(a > 25 && a < 50)
-{
-    printf("Third Class");
-}
-else if(a >= 0 && a < 35)
-{
-    printf("Fail");
-}
 return 0;
-
 }
